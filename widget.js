6bebed10125fbbccf268977f68b915afada2f6f5
Saïd HAMADOU <said.hamadou@gmail.com>
2026-03-02 15:18:23 +0100

    Separate Jodit toolbar from page: toolbarContainer + grid for rulers

--- widget.js
+++ widget.js
@@ -1615,6 +1615,7 @@ function initEditor() {
     placeholder: currentLang === 'fr' ? 'Commencez à écrire votre document ici...' : 'Start writing your document here...',
     allowResizeY: true,
     toolbarAdaptive: false,
+    toolbarContainer: '#jodit-toolbar-container',
     askBeforePasteHTML: false,
     askBeforePasteFromWord: false,
     defaultActionOnPaste: 'insert_clear_html',
